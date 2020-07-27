7f92a35723c8b1d8f22789225d2b79139fbdb866
srsexton94 <srsexton94@gmail.com>
2020-07-26 21:46:57 -0400

    Add new components to routing array

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { TrainingComponent } from './training/training.component';
+import { TimerComponent } from './timer/timer.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'training', component: TrainingComponent },
+  { path: 'timer', component: TimerComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
