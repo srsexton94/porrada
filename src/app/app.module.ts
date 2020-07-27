bb35b100344ffaff8621183e96cff092c2ffad8a
srsexton94 <srsexton94@gmail.com>
2020-07-26 21:47:13 -0400

    Adjust styling further

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TrainingComponent } from './training/training.component';
+import { TimerComponent } from './timer/timer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TrainingComponent,
+    TimerComponent
   ],
   imports: [
     BrowserModule,
