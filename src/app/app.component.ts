387418001fd8ba77b1c4de00ec8e1d94df14bb22
srsexton94 <srsexton94@gmail.com>
2020-07-28 11:32:30 -0400

    Refactored menu into own, new component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,11 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'porrada';
+  title: string;
+
+  constructor() {
+    this.title = 'Porrada'
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { TrainingComponent } from './training/training.component';
 import { TimerComponent } from './timer/timer.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     TrainingComponent,
-    TimerComponent
+    TimerComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
