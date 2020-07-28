3b9cb84c595ac3a27e36de2b1f64690a252a549b
srsexton94 <srsexton94@gmail.com>
2020-07-27 22:07:31 -0400

    Add code for functioning stopwatch

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -5,11 +5,56 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './timer.component.html',
   styleUrls: ['./timer.component.scss']
 })
+
 export class TimerComponent implements OnInit {
+
+  minutes: string = '00';
+  seconds: string = '00';
+  hundredths: string = '00';
+  btnStatus: string = 'Start';
+  running: boolean = false;
+  runTick: any
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  startTimer() {
+    this.hundredths = '00'
+    this.seconds = '00'
+    this.minutes = '00'
+    this.runTick = setInterval(() => {
+      let minNum: number = parseInt(this.minutes)
+      let secNum: number = parseInt(this.seconds)
+      let hundNum: number = parseInt(this.hundredths)
+
+      if (hundNum === 99) {
+          hundNum = 0
+          secNum += 1
+      } else {
+          hundNum += 1
+      }
+      if (secNum === 59) {
+          secNum = 0
+          minNum += 1
+      }
+
+      this.minutes = minNum < 10 ? '0' + minNum : minNum.toString()
+      this.seconds = secNum < 10 ? '0' + secNum : secNum.toString()
+      this.hundredths = hundNum < 10 ? '0' + hundNum : hundNum.toString()
+    }, 10)
+  }
+
+  startStopTimer() {
+    if (this.running) {
+      this.running = false
+      clearInterval(this.runTick)
+      this.btnStatus = 'Start'
+    } else {
+      this.running = true
+      this.startTimer()
+      this.btnStatus = 'Stop'
+    }
+  }
 }
