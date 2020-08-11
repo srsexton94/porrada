c135e9baf911461a8aa432789c733866c040b16e
srsexton94 <srsexton94@gmail.com>
2020-08-11 11:45:53 -0400

    Fix menu component

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -7,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class MenuComponent implements OnInit {
+  menuOpen: boolean = false
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  toggleMenu(): void {
+    this.menuOpen = !this.menuOpen
+  }
 }
